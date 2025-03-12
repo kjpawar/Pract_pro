d30e6eff463f773bcd46171bdf781c0e8412da84
kjpawar <kajalpawar61061@gmail.com>
2025-03-12 19:12:16 +0530

    code updated

--- pract_pro/urls.py
+++ pract_pro/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('helloWorld_app.urls')),
+    path('',include('pract_app.urls')),
 ]
